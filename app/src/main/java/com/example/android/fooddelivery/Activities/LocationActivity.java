1ab473b4828a32c3b1770d76121b80d97178bb11
Danny Wu <Danny Wu>
2016-08-01 12:25:51 +0800

    Create the project

---
+++ app/src/main/java/com/example/android/fooddelivery/Activities/LocationActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.fooddelivery.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.fooddelivery.R;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+
+
+}
